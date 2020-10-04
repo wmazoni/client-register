e284ea408ceca5ecbe6341ee5b13ff4c28f55220
Wellington Andrade <wmazoni@protonmail.com>
2020-10-04 10:26:31 -0300

    Client class created, configure and seed H2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Priscila Ana Novaes', '87759630271', 2500.0, TIMESTAMP WITH TIME ZONE '1983-07-14T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Ana Sebastiana Renata Sales', '17644746862', 1800.0, TIMESTAMP WITH TIME ZONE '1999-10-17T20:50:07.12345Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Mario Vinicius Castro', '63714396691', 3500.0, TIMESTAMP WITH TIME ZONE '1991-07-22T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jorge Giovanni Carlos Eduardo Aparicio', '01175070599', 4500.0, TIMESTAMP WITH TIME ZONE '1971-03-07T20:50:07.12345Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Luiz Marcio Monteiro', '64810040127', 1800.0, TIMESTAMP WITH TIME ZONE '1965-04-06T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Jessica Regina Almeida', '52686324260', 2200.0, TIMESTAMP WITH TIME ZONE '1969-02-09T20:50:07.12345Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Teresinha Maya da Costa', '21404314210', 2180.0, TIMESTAMP WITH TIME ZONE '1940-11-05T20:50:07.12345Z', 5);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Manuel Julio Sebastiao Oliveira', '03741736058', 3500.0, TIMESTAMP WITH TIME ZONE '1952-04-08T20:50:07.12345Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Mirella Juliana Isabelly Pires', '76829090806', 7500.0, TIMESTAMP WITH TIME ZONE '1973-02-02T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Leticia Clara Brito', '19751359031', 5500.0, TIMESTAMP WITH TIME ZONE '1990-05-25T20:50:07.12345Z', 0);
---
+++ src/test/java/com/wmazoni/costumer/CostumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wmazoni.costumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
